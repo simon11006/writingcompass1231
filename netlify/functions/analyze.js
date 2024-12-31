6b8e8c620b9ba23276545b46b40491557d4bb951
simon11006 <endeavor1006@naver.com>
2024-12-31 21:36:05 +0900

    Update analyze.js

--- netlify/functions/analyze.js
+++ netlify/functions/analyze.js
@@ -182,7 +182,7 @@ export const handler = async (event) => {
                                                                      - 평균 문장 길이: [숫자]자`
                                         }],
                                         temperature: 0.25,
-                                        max_tokens: 4000
+                                        max_tokens: 3000
                                       });
 
                                        return {
